7be54b694f16954eaa1f03d2f8e3cd1d27a0e704
MiltonRamos2002 <milton.ramos954@ist17dejulio.edu.ec>
2023-07-18 19:47:47 -0500

    Arualizacion de imagenes

--- src/main/java/com/mycompany/sistema_superm/SuperM.java
+++ src/main/java/com/mycompany/sistema_superm/SuperM.java
@@ -101,35 +101,35 @@ public class SuperM extends javax.swing.JFrame {
         lblCli9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Hombre.png"))); // NOI18N
         jPanel1.add(lblCli9, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 68, -1, -1));
 
-        lblCli10.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\customer_person_people_woman_you_1627 (1).png")); // NOI18N
+        lblCli10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Mujer.png"))); // NOI18N
         lblCli10.setText("jLabel2");
         jPanel1.add(lblCli10, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 221, 59, -1));
 
-        lblCli11.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\User-80_icon-icons.com_57249 (1).png")); // NOI18N
+        lblCli11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Hombre.png"))); // NOI18N
         lblCli11.setText("jLabel3");
         jPanel1.add(lblCli11, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 379, 72, -1));
 
-        lblCli12.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\customer_person_people_woman_you_1627 (1).png")); // NOI18N
+        lblCli12.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Mujer.png"))); // NOI18N
         lblCli12.setText("jLabel4");
         jPanel1.add(lblCli12, new org.netbeans.lib.awtextra.AbsoluteConstraints(16, 540, 62, -1));
 
-        lblCj1.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\cashier_machine_cash_register_pos_icon_225201.png")); // NOI18N
+        lblCj1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Caja.png"))); // NOI18N
         lblCj1.setText("jLabel5");
         jPanel1.add(lblCj1, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 80, 69, -1));
 
-        lblCj2.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\cashier_machine_cash_register_pos_icon_225201.png")); // NOI18N
+        lblCj2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Caja.png"))); // NOI18N
         lblCj2.setText("jLabel6");
         jPanel1.add(lblCj2, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 210, 69, -1));
 
-        lblCj3.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\cashier_machine_cash_register_pos_icon_225201.png")); // NOI18N
+        lblCj3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Caja.png"))); // NOI18N
         lblCj3.setText("jLabel7");
         jPanel1.add(lblCj3, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 380, 69, -1));
 
-        lblCj4.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\cashier_machine_cash_register_pos_icon_225201.png")); // NOI18N
+        lblCj4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Caja.png"))); // NOI18N
         lblCj4.setText("jLabel8");
         jPanel1.add(lblCj4, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 530, 69, -1));
 
-        lblSal.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\exit_door_cinema_icon_190851.png")); // NOI18N
+        lblSal.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Caja.png"))); // NOI18N
         lblSal.setText("jLabel9");
         jPanel1.add(lblSal, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 20, 60, -1));
 
@@ -145,50 +145,50 @@ public class SuperM extends javax.swing.JFrame {
         lblCli5.setText("jLabel1");
         jPanel1.add(lblCli5, new org.netbeans.lib.awtextra.AbsoluteConstraints(176, 72, 62, -1));
 
-        lblCli1.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\User-80_icon-icons.com_57249 (1).png")); // NOI18N
+        lblCli1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Hombre.png"))); // NOI18N
         lblCli1.setText("jLabel2");
         jPanel1.add(lblCli1, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 68, 67, -1));
 
-        lblCli6.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\User-80_icon-icons.com_57249 (1).png")); // NOI18N
+        lblCli6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Hombre.png"))); // NOI18N
         lblCli6.setText("jLabel3");
         jPanel1.add(lblCli6, new org.netbeans.lib.awtextra.AbsoluteConstraints(176, 217, 62, -1));
 
-        lblCli2.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\customer_person_people_woman_you_1627 (1).png")); // NOI18N
+        lblCli2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Mujer.png"))); // NOI18N
         lblCli2.setText("jLabel4");
         jPanel1.add(lblCli2, new org.netbeans.lib.awtextra.AbsoluteConstraints(338, 221, 59, -1));
 
-        lblCli7.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\customer_person_people_woman_you_1627 (1).png")); // NOI18N
+        lblCli7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Mujer.png"))); // NOI18N
         lblCli7.setText("jLabel5");
         jPanel1.add(lblCli7, new org.netbeans.lib.awtextra.AbsoluteConstraints(176, 383, 62, -1));
 
-        lblCli3.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\User-80_icon-icons.com_57249 (1).png")); // NOI18N
+        lblCli3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Hombre.png"))); // NOI18N
         lblCli3.setText("jLabel6");
         jPanel1.add(lblCli3, new org.netbeans.lib.awtextra.AbsoluteConstraints(338, 379, 69, -1));
 
-        lblCli8.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\User-80_icon-icons.com_57249 (1).png")); // NOI18N
+        lblCli8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Hombre.png"))); // NOI18N
         lblCli8.setText("jLabel7");
         jPanel1.add(lblCli8, new org.netbeans.lib.awtextra.AbsoluteConstraints(176, 526, 62, -1));
 
-        lblCli4.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\customer_person_people_woman_you_1627 (1).png")); // NOI18N
+        lblCli4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Mujer.png"))); // NOI18N
         lblCli4.setText("jLabel8");
         jPanel1.add(lblCli4, new org.netbeans.lib.awtextra.AbsoluteConstraints(338, 530, 59, -1));
 
-        lblS1.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\User-80_icon-icons.com_57249 (1).png")); // NOI18N
+        lblS1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Hombre.png"))); // NOI18N
         jPanel1.add(lblS1, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 90, -1, -1));
 
-        lblS2.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\customer_person_people_woman_you_1627 (1).png")); // NOI18N
+        lblS2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Mujer.png"))); // NOI18N
         jPanel1.add(lblS2, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 180, -1, -1));
 
-        lblS3.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\User-80_icon-icons.com_57249 (1).png")); // NOI18N
+        lblS3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Hombre.png"))); // NOI18N
         jPanel1.add(lblS3, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 270, -1, -1));
 
-        lblS4.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\customer_person_people_woman_you_1627 (1).png")); // NOI18N
+        lblS4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Mujer.png"))); // NOI18N
         jPanel1.add(lblS4, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 370, 50, 70));
 
-        lblS5.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\User-80_icon-icons.com_57249 (1).png")); // NOI18N
+        lblS5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Hombre.png"))); // NOI18N
         jPanel1.add(lblS5, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 460, 70, -1));
 
-        lblS6.setIcon(new javax.swing.ImageIcon("C:\\Users\\MSI-THIN\\Downloads\\customer_person_people_woman_you_1627 (1).png")); // NOI18N
+        lblS6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Mujer.png"))); // NOI18N
         jPanel1.add(lblS6, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 550, 70, 70));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
